c90aa6dee1af461cad2efa550b86873dd95116f2
Akgullll <161600237+Akgullll@users.noreply.github.com>
2024-06-01 03:45:48 +0200

    footer.php

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,21 +1,16 @@
-<?php
-$page_name = basename($_SERVER["SCRIPT_NAME"], '.php');
-$page_object = new Page();
-$page_object->set_page_name($page_name);
-?>
 <footer class="container " style="background-color: pink">
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-25">
         <h2>we</h2>
         <p>Apple je neuveriteľným príkladom inovácie v oblasti technológií.</p>
         <p>Vytvárame zariadenia, ktoré sú nielen funkčné, ale aj dizajnovo atraktívne.</p>
       </div>
-      <div class="col-md-3t">
+      <div class="col-25">
         <h2>any questions?</h2>
         <p><i class="fa fa-envelope" aria-hidden="true"><a href="mailto:Apple@gmail.com"> Apple@gmail.com</a></i></p>
         <p><i class="fa fa-phone" aria-hidden="true"><a href="tel:0342345656"> 0342345656</a></i></p>
       </div>
-      <div class="col-md-3">
+      <div class="col-25">
         <h2>address</h2>
         <a href="https://maps.app.goo.gl/BKn9n9GWD16pK7xw7" class="button">APPLE</a>
        </div>
@@ -25,7 +20,10 @@ $page_object->set_page_name($page_name);
     </div>
   </footer>
     <?php
-      echo($page_object->add_scripts());
+    $page_name = basename($_SERVER["SCRIPT_NAME"], '.php');
+    $page_object = new Page();
+    $page_object->set_page_name($page_name);
+    echo($page_object->add_scripts());
     ?>
 </body>
 </html>
